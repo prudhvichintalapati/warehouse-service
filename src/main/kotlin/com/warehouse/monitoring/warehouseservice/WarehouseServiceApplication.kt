2cfda30bebd98fd1731d9c9170c19a9ea0397a8d
Prudhvi <prudhvi.chintalapati@thalesdigital.io>
2026-02-08 17:28:55 +0800

    Initial Spring Boot setup

---
+++ src/main/kotlin/com/warehouse/monitoring/warehouseservice/WarehouseServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.warehouse.monitoring.warehouseservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/warehouse/monitoring/warehouseservice/WarehouseServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.warehouse.monitoring.warehouseservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
